88b08db512d91817cb5ce6dae5dc6dc6582c8e84
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2016-07-21 21:33:13 -0700

    non-conflicting files list

--- src/devel/version.mk
+++ src/devel/version.mk
@@ -2,3 +2,4 @@ NAME = rocks-devel
 RELEASE = 3
 RPM.EXTRAS="%define _use_internal_dependency_generator 0\\n%define __find_requires %{_builddir}/%{name}-%{version}/filter-requires.sh"
 RPM.REQUIRES=createrepo
+RPM.FILESLIST=filelist
