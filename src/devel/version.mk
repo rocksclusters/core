45e59f1f09f0e6f88f5e69543285ad03ff3f9074
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2016-10-05 10:22:44 -0700

    Cleaner build of rocks-devel package to use just the RPM.FILES tag and filtering out /opt/rocks/python automatically-generated dependency

--- src/devel/version.mk
+++ src/devel/version.mk
@@ -3,4 +3,3 @@ RELEASE = 6
 RPM.FILES="/etc/profile.d/*\\n/opt/rocks/share/devel/*"
 RPM.EXTRAS="%define _use_internal_dependency_generator 0\\n%define __find_requires %{_builddir}/%{name}-%{version}/filter-requires.sh"
 RPM.REQUIRES=createrepo
-RPM.FILESLIST=filelist
