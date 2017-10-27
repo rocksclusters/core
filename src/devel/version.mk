4950cc0bdbb1eaf205c9320b1d95edd2bee351d6
Philip Papadopulos <ppapadopoulos@ucsd.edu>
2017-10-27 17:17:40 -0400

    Support an include/json directory so that attributes can be added to the GUI installer (sort of like screens from rocks5/6 but simpler)

--- src/devel/version.mk
+++ src/devel/version.mk
@@ -1,5 +1,5 @@
 NAME = rocks-devel
-RELEASE = 7
+RELEASE = 8
 RPM.FILES="/etc/profile.d/*\\n/opt/rocks/share/devel/*"
 RPM.EXTRAS="%define _use_internal_dependency_generator 0\\n%define __find_requires %{_builddir}/%{name}-%{version}/filter-requires.sh"
 RPM.REQUIRES=createrepo
