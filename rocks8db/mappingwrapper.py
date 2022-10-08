ba99ebea3d504fdcbc76d2717c7aa37e08fb2d34
Philip M. Papadopoulos <ppapadop@uci.edu>
2022-10-07 17:20:03 -0700

    Slowly simplifying the sqlalchemy interface. Makefile produces "mapping.py" with the mapping wrapper (similar to older implementation) in the right place. mapping.py uses sqlacodegen to read the database structure and create the class structure

---
+++ rocks8db/mappingwrapper.py
@@ -0,0 +1,43 @@
+class RocksBase(object):
+    """Additional base class of Rocks ORM hierarchy which includes some
+    helper methods for all classes"""
+
+    @property
+    def session(self):
+        """Singelton which return the session of the object"""
+        return sqlalchemy.orm.session.object_session(self)
+
+
+    def delete(self):
+        """instance method to autodelete the instance which calls it
+
+        so you can use
+        node.delete()"""
+        self.session.delete(self)
+
+    @classmethod
+    def loadOne(cls, session, **kwargs):
+        """ """
+        return cls.load(session, **kwargs).one()
+
+
+    @classmethod
+    def load(cls, session, **kwargs):
+        """
+        this method allow us to run query on all the mapping objects
+        simply using 
+
+        e.g.::
+
+          node = Nodes.load(session, Name='compute-0-0', Cpus=2)
+          nic = Network.load(session, Name='compute-0-0', Interface='eth0')
+
+        taken from:
+        http://petrushev.wordpress.com/2010/06/22/sqlalchemy-base-model/
+        """
+        q = session.query(cls)
+        filters = [getattr(cls, field_name)==kwargs[field_name] \
+                for field_name in kwargs]
+        return q.filter(and_(*filters))
+
+
--- rocks8db/testdb.py
+++ rocks8db/testdb.py
@@ -1,143 +1,20 @@
 #!/usr/bin/env python3
 from sqlalchemy import create_engine
-from sqlalchemy import Table,Column,Integer,func 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 
-engine = create_engine('sqlite:///rocks8.db', echo=True)
-Base = declarative_base(engine)
-########################################################################
-class Bootactions(Base): 
-     __tablename__ = 'bootactions' 
-     __table_args__ = {'autoload':True}
-  
-class Distributions(Base): 
-     __tablename__ = 'distributions' 
-     __table_args__ = {'autoload':True}
-  
-class Netgens(Base): 
-     __tablename__ = 'netgens' 
-     __table_args__ = {'autoload':True}
-  
-class Reslevels(Base): 
-     __tablename__ = 'reslevels' 
-     __table_args__ = {'autoload':True}
-  
-class Rolls(Base): 
-     __tablename__ = 'rolls' 
-     __table_args__ = {'autoload':True}
-  
-class Subnets(Base): 
-     __tablename__ = 'subnets' 
-     __table_args__ = {'autoload':True}
-  
-class Vlans(Base): 
-     __tablename__ = 'vlans' 
-     __table_args__ = {'autoload':True}
-  
-class Appliances(Base): 
-     __tablename__ = 'appliances' 
-     __table_args__ = {'autoload':True}
-  
-class Attrs(Base): 
-     __tablename__ = 'attrs' 
-     __table_args__ = {'autoload':True}
-  
-class Firewalls(Base): 
-     __tablename__ = 'firewalls' 
-     __table_args__ = {'autoload':True}
-  
-class Nodegroups(Base): 
-     __tablename__ = 'nodegroups' 
-     __table_args__ = {'autoload':True}
-  
-class Nodes(Base): 
-     __tablename__ = 'nodes' 
-     __table_args__ = {'autoload':True}
-  
-class Partitions(Base): 
-     __tablename__ = 'partitions' 
-     __table_args__ = {'autoload':True}
-  
-class Routes(Base): 
-     __tablename__ = 'routes' 
-     __table_args__ = {'autoload':True}
-  
-class Bootflags(Base): 
-     __tablename__ = 'bootflags' 
-     __table_args__ = {'autoload':True}
-  
-class Groupmembers(Base): 
-     __tablename__ = 'groupmembers' 
-     __table_args__ = {'autoload':True}
-  
-class Hwinventory(Base): 
-     __tablename__ = 'hwinventory' 
-     __table_args__ = {'autoload':True}
-  
-class Netdevs(Base): 
-     __tablename__ = 'netdevs' 
-     __table_args__ = {'autoload':True}
-  
-class Ipaddrs(Base): 
-     __tablename__ = 'ipaddrs' 
-     __table_args__ = {'autoload':True}
-  
-class Allattrsview(Base): 
-     __table__ = Table( 'allattrsview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Attrsview(Base): 
-     __table__ = Table( 'attrsview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Firewallsview(Base): 
-     __table__ = Table( 'firewallsview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Groupmembersview(Base): 
-     __table__ = Table( 'groupmembersview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Hwinventoryview(Base): 
-     __table__ = Table( 'hwinventoryview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Netdevsview(Base): 
-     __table__ = Table( 'netdevsview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Nodesview(Base): 
-     __table__ = Table( 'nodesview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Partitionsview(Base): 
-     __table__ = Table( 'partitionsview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Routesview(Base): 
-     __table__ = Table( 'routesview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-class Vlansview(Base): 
-     __table__ = Table( 'vlansview', Base.metadata, Column('id',Integer, primary_key=True), 
-                          autoload=True, autoload_with=engine) 
-
-#----------------------------------------------------------------------
-def loadSession():
-    """"""
-    metadata = Base.metadata
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
-    
+from mapping import *
+from database import *
 if __name__ == "__main__":
-    session = loadSession()
-    attrs = session.query(func.upper(Allattrsview.node).label('node'),Allattrsview.attr,Allattrsview.value,
-      Allattrsview.shadow,Allattrsview.resname,func.max(Allattrsview.level).label('level')).\
-      group_by(Allattrsview.node,Allattrsview.attr).\
-      all()
-    for a in attrs:
-        print (a.node, a.attr, a.value, a.shadow, a.resname,a.level)
+    db = Database()
+    db.setVerbose(True)
+    db.connect()
+    print (db)
+    TAV=t_attrsview
+    query = select([TAV.c.attr,TAV.c.value,TAV.c.resname]).where(TAV.c.reskey == "compute-0-2")
+
+    for a in db.engine.execute(query).fetchall():
+        print (a)
     #for i in range(0,len(attrs)):
     #    print (attrs[i].node, attrs[i].attr, attrs[i].value, attrs[i].shadow, attrs[i].resname,attrs[i].level)
